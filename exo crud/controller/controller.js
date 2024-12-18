efd9840ed091f5fe05197ee81009943d36560cd1
DaudevInfo <sebastien.daudey@gmail.com>
2024-12-18 10:02:01 +0100

    TP acces DB

--- exo crud/controller/controller.js
+++ exo crud/controller/controller.js
@@ -1,48 +1,53 @@
-const { Users } = require("../model/Users")
+const { UserDb } = require("../model/Users")
 const controller = {}
 
 
-
-
-controller.index =  (req, res) => {
+controller.index =  async(req, res) => {
     const {page,limit} = req.query
     console.log("page" + page + "limit" + limit)
     console.log("page" + parseInt(limit) + "limit" + parseInt(page))
+    let UsersDb = await UserDb.findAll()
 
     if (parseInt(limit) >0 && parseInt(page) >0) {
-        nbPages = Math.floor(Users.length/limit)
-        const UsersPage = Users.slice((page-1)*limit, page*limit)
+        nbPages = Math.floor(UsersDb.length/limit)
+        const UsersPage = UsersDb.slice((page-1)*limit, page*limit)
         res.send(UsersPage)
     } else {  
-        res.send(Users)
+        console.log("UsersDb" + UsersDb)
+        res.send(UsersDb)
     }     
  }
     
-controller.search = (req, res) => {
+controller.search = async(req, res) => {
     const name = req.params.name
     console.log("name" +name)
     console.log("name" + name.toUpperCase())
     
-    const User = Users.filter(user => user.name.toLocaleUpperCase().includes(name.toUpperCase()) ) 
+    const User = await UserDb.findAll({where : {name : name}})
+    
+    console.log("user" + User)
+    //Users.filter(user => user.name.toLocaleUpperCase().includes(name.toUpperCase()) ) 
     res.send(User)
 
 }
 
-controller.show = (req, res) => {
+controller.show = async(req, res) => {
     const id = req.params.id
     console.log("id" +id)
-    const User = Users.find(user => user.id == id)
-    res.send(User)
+    const user = await UserDb.findOne({where : {id : id}})
+    if (user) { res.send(user)}
+    else {res.status(404).json({message: "User not found"})}
+
 
 }
 
 
 
 
-controller.create = (req, res) => {
+controller.create = async(req, res) => {
     let {name,age} = req.body
-    const idNumber = Users.reduce((acc, user) => acc <= user.id ? user.id+1 : acc, 0)
-    const user = {name,age,id:idNumber}    
+    //const idNumber = Users.reduce((acc, user) => acc <= user.id ? user.id+1 : acc, 0)
+    //const user = {name,age,id:idNumber}    
 
     age = parseInt(age)
     console.log("nom"+ name)
@@ -52,7 +57,7 @@ controller.create = (req, res) => {
     
     
     if (name.length !=0 && Number.isInteger(age) && age > 0) { 
-        Users.push(user)
+        await UserDb.create({name:name, age:age})
         res.status(201).json({user, message: "User created succesfully!"})
     } else {
         res.status(400).json({message: "User not created!"})
@@ -60,31 +65,30 @@ controller.create = (req, res) => {
     
 }
 
-controller.update = (req, res) => {
+controller.update = async (req, res) => {
     let {id,name,age} = req.body
     
     age = parseInt(age)
-    const user = {name,age,id:parseInt(id)}
-    userExist = Users.find(user => user.id == id)
+    //const user = {name,age,id:parseInt(id)}
+    userExist = await UserDb.findOne({where: {id:id}})
    
     if (userExist && name.length !=0  && Number.isInteger(age) && age > 0) {
-        Users = Users.filter(user => user.id != id)
-        Users.push(user)
-        res.status(200).json({user, message: "User updated succesfully!"})
+        await UserDb.update({name:name, age:age}, {where: {id:id}})
+        res.status(200).json({message: "User updated succesfully!"})
     }
     else {
         res.status(404).json({message: "User not found"})
     }
 }
 
- controller.destroy = (req, res) => {
+ controller.destroy = async(req, res) => {
     const {id,name,age} = req.body
     const user = {name,age,id:parseInt(id)}
-    userExist = Users.find(user => user.id == id)
-   
+    //userExist = Users.find(user => user.id == id)
+    userExist = await UserDb.findOne({where: {id:id}})
     
     if (userExist) {
-        Users = Users.filter(user => user.id != id)
+        UserDb.destroy({where: {id:id}})
         res.status(200).json({user, message: "User deleted succesfully!"})
     }
     else {
